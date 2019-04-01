e67593e50d63c1e183b0b7dca79d77fe6cbec5c8
Shritesh <shritesh.sj@gmail.com>
2019-04-02 00:34:03 +0530

    Updates

---
+++ precache-manifest.ac01c021ffc2a85ae0e75a6f566ea52f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09d4bc5f1dd6a3b15090",
+    "url": "/ameiim-2019/static/css/main.e374c562.chunk.css"
+  },
+  {
+    "revision": "09d4bc5f1dd6a3b15090",
+    "url": "/ameiim-2019/static/js/main.8bd8027f.chunk.js"
+  },
+  {
+    "revision": "a5445edc2077ac2c2a61",
+    "url": "/ameiim-2019/static/js/runtime~main.cbc4e9d9.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ameiim-2019/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ee7129254b1374acbb8a495f5c96106a",
+    "url": "/ameiim-2019/index.html"
+  }
+];
