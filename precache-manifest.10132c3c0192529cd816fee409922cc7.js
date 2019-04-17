9bae7abe0280b194fdbd22098cb5a83dcbe5a740
Shritesh <shritesh.sj@gmail.com>
2019-04-17 14:36:19 +0530

    Updates

---
+++ precache-manifest.10132c3c0192529cd816fee409922cc7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d969025ae2463908110e",
+    "url": "/AMEIIM-2019/static/css/main.e5418dda.chunk.css"
+  },
+  {
+    "revision": "d969025ae2463908110e",
+    "url": "/AMEIIM-2019/static/js/main.f8711425.chunk.js"
+  },
+  {
+    "revision": "e12e1bf6e81d69e8cf2a",
+    "url": "/AMEIIM-2019/static/js/runtime~main.713b828d.js"
+  },
+  {
+    "revision": "b4a1aaca2b551ebfbe6e",
+    "url": "/AMEIIM-2019/static/css/2.3f9a63ce.chunk.css"
+  },
+  {
+    "revision": "b4a1aaca2b551ebfbe6e",
+    "url": "/AMEIIM-2019/static/js/2.ef4fc291.chunk.js"
+  },
+  {
+    "revision": "c1d12f040abd420f9c289ba30542703c",
+    "url": "/AMEIIM-2019/static/media/nit.c1d12f04.jpeg"
+  },
+  {
+    "revision": "d15168e684c343e0257d4d19a824d7a1",
+    "url": "/AMEIIM-2019/static/media/Nitrr.d15168e6.jpeg"
+  },
+  {
+    "revision": "5a8e7b187d134a2f24526f5597b47b16",
+    "url": "/AMEIIM-2019/static/media/logo4.5a8e7b18.jpeg"
+  },
+  {
+    "revision": "6896dc083c3ac676e1301c156fd002f9",
+    "url": "/AMEIIM-2019/static/media/raipur.6896dc08.jpeg"
+  },
+  {
+    "revision": "9583e7623596fc079b0d492480963244",
+    "url": "/AMEIIM-2019/static/media/logo1.9583e762.jpeg"
+  },
+  {
+    "revision": "5ab17829ae094af2a08eb40bf7914a90",
+    "url": "/AMEIIM-2019/static/media/Letter HEad.5ab17829.JPG"
+  },
+  {
+    "revision": "241e7228d1df73b9ee11e5c86662db17",
+    "url": "/AMEIIM-2019/static/media/easychair.241e7228.png"
+  },
+  {
+    "revision": "43d46116bb9dc8350b5b19391558e854",
+    "url": "/AMEIIM-2019/static/media/Dr_Rawani.43d46116.jpeg"
+  },
+  {
+    "revision": "be7d776a2db8f6e4a52dcdba3c77003d",
+    "url": "/AMEIIM-2019/static/media/prof.karunajain.be7d776a.jpeg"
+  },
+  {
+    "revision": "c4b081e975b5a5c254d6668b57e35e5e",
+    "url": "/AMEIIM-2019/static/media/logo.c4b081e9.svg"
+  },
+  {
+    "revision": "e5f105a71dab3557d0950c86022f083b",
+    "url": "/AMEIIM-2019/index.html"
+  }
+];
