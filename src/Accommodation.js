dda144cd933b03f3beb61b09826d12871b6ed088
Shritesh <shritesh.sj@gmail.com>
2019-04-17 22:32:27 +0530

    completed accmodation

--- src/Accommodation.js
+++ src/Accommodation.js
@@ -4,16 +4,16 @@ class Accommodations extends Component {
   render() {
     return (
       <section id="committee" className="hero section">
-              <div className=" has-text-centered">
-                  <a href="#">
-                      <div className="button is-success is-large">
-                          <div className="font subtitle is-4 has-text-white">
-                              Download Brochure
-                          </div>
-                      </div>
-                  </a>
+        <div className=" has-text-centered">
+          <a href="#">
+            <div className="button is-success is-large">
+              <div className="font subtitle is-4 has-text-white">
+                Download Brochure
               </div>
-          <br/>
+            </div>
+          </a>
+        </div>
+        <br />
         <div className="hero-head">
           <div className="container has-text-centered">
             <div className="title is-3 heading">
@@ -23,26 +23,74 @@ class Accommodations extends Component {
         </div>
         <div className="hero-body">
           <div className="container">
-              <div className="font subtitle is-4 has-text-black">
-                  <a href="http://www.nitrr.ac.in/guest_house.php" className="has-text-black" target="_blank"> Institute Guest House</a>
+            <div className="columns">
+              <div className="column">
+                <div className="columns">
+                  <div className="column">
+                    <div className="font subtitle is-4 has-text-black">
+                      <a
+                        href="http://www.nitrr.ac.in/guest_house.php"
+                        className="has-text-black und"
+                        target="_blank"
+                      >
+                        Institute Guest House:
+                      </a>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="font subtitle is-4">
+                      <a href="mainto:guesthouse@nitrr.ac.in">
+                        <i className="fas fa-envelope fa-lg" />
+                        {"  "}
+                        guesthouse@nitrr.ac.in
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <br />
+                <div className="columns">
+                  <div className="column">
+                    <div className="font subtitle is-4 has-text-black">
+                      Boys hostel:
+                      <br />
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="font subtitle is-4">
+                        Dr. XXXXXXXXXX<br/>
+                        Warden <br/>
+                        <a href="tel:+91XXXXXXXXXX"><i className="fas fa-phone fa-lg" />{"  "}+91-XXXXXXXXXX </a><br/>
+                        <a href="mainto:xxxxxxx@nitrr.ac.in">
+                        <i className="fas fa-envelope fa-lg" />
+                        {"  "}
+                            xxxxxxxxx@nitrr.ac.in</a> <br/>
+                        Nearly 10 seats are available.<br />
+                    </div>
+                  </div>
+                </div>
+                <br />
+                <div className="columns">
+                  <div className="column">
+                    <div className="font subtitle is-4 has-text-black">
+                      Ladies hostel:
+                      <br />
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="font subtitle is-4">
+                        Dr. XXXXXXXXXX<br/>
+                        Warden <br/>
+                        <a href="tel:+91XXXXXXXXXX"><i className="fas fa-phone fa-lg" />{"  "}+91-XXXXXXXXXX </a><br/>
+                        <a href="mainto:xxxxxxxxxx@nitrr.ac.in">
+                            <i className="fas fa-envelope fa-lg" />
+                            {"  "}
+                            xxxxxxxxx@nitrr.ac.in</a> <br/>
+                      Only 2 double seated rooms are available<br/>
+                    </div>
+                  </div>
+                </div>
               </div>
-            <div className="font subtitle is-4">
-                Contact Details:
-                <br/><a href="mainto:guesthouse@nitrr.ac.in">guesthouse@nitrr.ac.in</a>
-            </div>
-              <br/>
-              <div className="font subtitle is-4 has-text-black">
-                  Boys hostel<br/>
-              </div>
-              <div className="font subtitle is-4">
-                  Nearly 10 seats are available.
-              </div>
-            <br/>
-            <div className="font subtitle is-4 has-text-black">
-                Ladies hostel<br/>
-            </div>
-            <div className="font subtitle is-4">
-                Only 2 double seated rooms are available
+              <div className="column" />
             </div>
           </div>
         </div>
